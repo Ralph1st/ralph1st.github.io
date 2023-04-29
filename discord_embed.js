bc14a4363519fe3b5a30ea44b51d816fe310009f
Ralph1st <raphael.perinet@hotmail.com>
2023-04-29 10:26:27 +0200

    js

---
+++ discord_embed.js
@@ -0,0 +1,14 @@
+console.log('JavaScript code is running!');
+
+const Discord = require('discord.js');
+
+const inviteEmbed = new Discord.MessageEmbed()
+    .setTitle('Join Our Discord Server!')
+    .setDescription('Click the link below to join our Discord community!')
+    .setColor('#7289da')
+    .addField('Invite Link', 'https://discord.gg/A8sFpeeZzu');
+
+const embedContainer = document.getElementById('discord-embed');
+embedContainer.appendChild(inviteEmbed);
+
+console.log('JavaScript code has ran !');
